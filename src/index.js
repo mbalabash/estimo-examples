489b641fdde6e013c07a23aa4f1256f63c4b9d2a
mbalabash <maksim.balabash@gmail.com>
2019-04-14 21:09:52 +0300

    Add low cpu experiment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
 const path = require('path')
 const estimo = require('estimo')
+const {
+  getFileSize, median, delay, randInt, writeJson,
+} = require('./utils')
 
 const libs = [
   path.resolve(path.join(__dirname, '..', 'libs', 'angular.1.7.8.min.js')),
@@ -7,9 +12,39 @@ const libs = [
   path.resolve(path.join(__dirname, '..', 'libs', 'react.16.8.6.production.min.js')),
   path.resolve(path.join(__dirname, '..', 'libs', 'redux.4.0.1.min.js')),
   path.resolve(path.join(__dirname, '..', 'libs', 'jquery-3.4.0.min.js')),
+  path.resolve(path.join(__dirname, '..', 'libs', 'antd.3.16.3.min.js')),
+  path.resolve(path.join(__dirname, '..', 'libs', 'aurelia-core.1.0.2.min.js')),
+  path.resolve(path.join(__dirname, '..', 'libs', 'backbone-min.1.4.0.js')),
+  path.resolve(path.join(__dirname, '..', 'libs', 'bluebird.core.3.5.4.min.js')),
+  path.resolve(path.join(__dirname, '..', 'libs', 'clipboard.2.0.4.min.js')),
 ]
 
+const countOfExperiments = 5
+
+const makeExperiment = async (perfCliArgs) => {
+  const estimations = {}
+
+  for (const lib of libs) {
+    const metrics = []
+    const key = path.basename(lib)
+    estimations[key] = { size: `${getFileSize(lib).toFixed(2)}kB`, average: -1 }
+
+    for (let i = 0; i < countOfExperiments; i += 1) {
+      await delay(randInt(25, 200))
+      const { javaScript } = await estimo(lib, perfCliArgs)
+      metrics.push(parseInt(javaScript, 10))
+    }
+
+    estimations[key].average = `${median(metrics)}ms`
+  }
+
+  return estimations
+}
+
 ;(async () => {
-  const report = await estimo(libs[0], ['--set-cpu-throttling-rate', '--rate', '4'])
-  console.log(report)
+  const results = await makeExperiment()
+  writeJson(path.join(__dirname, '..', 'reports', 'results.json'), results)
+
+  const resultsWithLowCPU = await makeExperiment(['--set-cpu-throttling-rate', '--rate', '4'])
+  writeJson(path.join(__dirname, '..', 'reports', 'resultsWithLowCPU.json'), resultsWithLowCPU)
 })()
---
+++ src/utils.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const jsonfile = require('jsonfile')
+
+const getFileSize = (filename) => {
+  const stats = fs.statSync(filename)
+  const fileSizeInBytes = stats.size
+  return fileSizeInBytes / 1024
+}
+
+const median = (array) => {
+  const sortedArray = array.sort((a, b) => a - b)
+  const { length } = sortedArray
+  const mid = parseInt(length / 2, 10)
+  if (length % 2) {
+    return sortedArray[mid]
+  }
+  const low = mid - 1
+  const hight = mid
+  return (sortedArray[low] + sortedArray[hight]) / 2
+}
+
+const randInt = (min, max) => Math.floor(Math.random() * max) + min
+
+const delay = async time => new Promise(resolve => setTimeout(resolve, time))
+
+const writeJson = (fileName, content) => {
+  jsonfile.writeFileSync(fileName, content, { spaces: 2 })
+}
+
+module.exports = {
+  getFileSize,
+  writeJson,
+  randInt,
+  median,
+  delay,
+}
