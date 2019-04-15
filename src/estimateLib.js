1c10d226645de0bfe0f7010403824d557ce63a20
Maksim Balabash <maksim.balabash@gmail.com>
2019-04-15 12:06:42 +0300

    Estimate js compile and execution

--- src/estimateLib.js
+++ src/estimateLib.js
@@ -2,10 +2,10 @@ const estimo = require('estimo')
 
 const estimateLib = async (lib, perfCliArgs) => {
   const start = Date.now()
-  const { javaScript } = await estimo(lib, perfCliArgs)
+  const { javaScript, javaScriptCompile } = await estimo(lib, perfCliArgs)
   const finish = Date.now()
 
-  const compileTime = parseFloat(javaScript).toFixed(2)
+  const compileTime = parseFloat(javaScript + javaScriptCompile).toFixed(2)
   const estimoDuration = parseFloat((finish - start) / 1000).toFixed(2)
 
   return {
