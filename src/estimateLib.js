c42abf87edc189ecf52d973400daddeba8a0c8de
Maksim Balabash <maksim.balabash@gmail.com>
2019-04-17 16:31:51 +0300

    Change way how estimo runs (to spawn process)

--- src/estimateLib.js
+++ src/estimateLib.js
@@ -1,8 +1,28 @@
-const estimo = require('estimo')
+const { npx } = require('node-npx')
 
-const estimateLib = async (lib, perfCliArgs) => {
+const spawnEstimoProcess = args => new Promise((resolve, reject) => {
+  const proc = npx('estimo', args)
+  let result = null
+
+  proc.stdout.on('data', (data) => {
+    const stringContent = data.toString()
+    if (!stringContent.includes('Done')) {
+      result = {}
+      eval(`result=${stringContent}`) // eslint-disable-line no-eval
+    }
+  })
+
+  proc.on('close', (code) => {
+    if (code === 0) resolve(result)
+    else {
+      reject(new Error(`Can't execute 'estimo'\nArgs: ${JSON.stringify(args)}`))
+    }
+  })
+})
+
+const estimateLib = async (lib, perfCliArgs = []) => {
   const start = Date.now()
-  const { javaScript, javaScriptCompile } = await estimo(lib, perfCliArgs)
+  const { javaScript, javaScriptCompile } = await spawnEstimoProcess(['-l', lib, perfCliArgs])
   const finish = Date.now()
 
   const compileTime = parseFloat(javaScript + javaScriptCompile).toFixed(2)
